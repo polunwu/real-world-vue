76907b7db0f9dbb9dd1e4d46ec45f3dc96bc299d
polunwu <polunwu@gmail.com>
2020-06-25 09:57:53 +0800

    Add view components for different routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate";
+import EventList from "../views/EventList";
+import EventShow from "../views/EventShow";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/event",
+    name: "event-show",
+    component: EventShow
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate
   }
 ];
 
