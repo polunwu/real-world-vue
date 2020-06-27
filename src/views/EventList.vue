d7cd9c03d51e9d492b6268b6bdf3cb38746672a2
polunwu <polunwu@gmail.com>
2020-06-28 06:33:54 +0800

    Organizing vuex with namespaced modules -> user, event

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event"></EventCard>
     <template v-if="page != 1">
       <router-link :to="{ name: 'event-list', query: { page: page - 1 }}" rel="prev">Prev Page</router-link>&nbsp;|&nbsp;
     </template>
@@ -20,7 +20,7 @@ export default {
     EventCard
   },
   created() {
-    this.$store.dispatch('fetchEvents', {
+    this.$store.dispatch('event/fetchEvents', {
       perPage: 3,
       page: this.page
     })
@@ -30,9 +30,9 @@ export default {
       return parseInt(this.$route.query.page) || 1
     },
     hasNextPage() {
-      return this.eventsTotal > this.page * 3
+      return this.event.eventsTotal > this.page * 3
     },
-    ...mapState(['events', 'eventsTotal'])
+    ...mapState(['event', 'user'])
   }
 }
 </script>
