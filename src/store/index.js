d0f2086602232547b20fe8af3c32078aa8f41c7d
polunwu <polunwu@gmail.com>
2020-06-27 20:59:06 +0800

    Refactor to fetchEvents via vuex Add pagination feature

--- src/store/index.js
+++ src/store/index.js
@@ -8,17 +8,34 @@ export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Adam Jahr'},
     categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
-    events: []
+    events: [],
+    eventsTotal: 0
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal
     }
   },
   actions: {
     createEvent({ commit }, event) {
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
+      })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+      .then(response => {
+        commit('SET_EVENTS_TOTAL', response.headers['x-total-count'])
+        commit('SET_EVENTS', response.data)
+      })
+      .catch(error => {
+        console.log('There was an error: ', error.response)
       })
     }
   },
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,30 +2,37 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1 }}" rel="prev">Prev Page</router-link>&nbsp;|&nbsp;
+    </template>
+    <template v-if="hasNextPage">
+      <router-link :to="{ name: 'event-list', query: { page: page + 1 }}" rel="next">Next Page</router-link>
+    </template>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log('There was an error: ', error.response)
-      })
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    hasNextPage() {
+      return this.eventsTotal > this.page * 3
+    },
+    ...mapState(['events', 'eventsTotal'])
   }
 }
 </script>
